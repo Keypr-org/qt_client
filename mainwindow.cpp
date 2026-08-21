c37967fd5093e21d878d7690326f6e16bda7cb56
WTHIJ <119924362+WTHIJ@users.noreply.github.com>
2026-08-21 18:48:02 +0200

    [Fix]: Pipeline seems to be working

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,22 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include "CryptoService.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    try {
+        // Test d'intégration keypr-core / libsodium
+        CryptoService crypto;
+        qDebug() << "keypr-core chargé avec succès depuis qt_client !";
+        qDebug() << "libsodium initialisée avec succès";
+    } catch (const std::exception& e) {
+        qWarning() << "Erreur lors de l'initialisation :" << e.what();
+    }
 }
 
 MainWindow::~MainWindow()
